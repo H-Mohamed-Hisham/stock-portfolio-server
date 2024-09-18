1036b816c306d688a551bc2ca45beabbc8ac3985
H-Mohamed-Hisham <hmhwebdev@gmail.com>
2024-09-18 16:32:40 +0530

    Transaction Entity Updated

--- src/transaction/entities/transaction.entity.ts
+++ src/transaction/entities/transaction.entity.ts
@@ -29,7 +29,7 @@ export class TransactionEntity implements Transaction {
   @ApiProperty()
   quantity: number;
 
-  @ApiProperty()
+  @ApiProperty({ type: AssetEntity })
   asset: AssetEntity;
 
   @ApiProperty()
